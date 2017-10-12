fb458e3ebcf86061fb86c58a63cde71d7f3655ab
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-10-11 18:50:52 -0700

    footprints and snps select and display

---
+++ R/trenaGWAS01.R
@@ -0,0 +1,254 @@
+library(SNPlocs.Hsapiens.dbSNP150.GRCh38)
+
+.trenaGWAS01 <- setClass("trenaGWAS01",
+                      representation=representation(
+                         dataSummary="character",
+                         gwasLocusNumber="numeric",
+                         targetGene="character",
+                         targetGene.tss="numeric",
+                         trena="Trena",
+                         trenaViz="trenaViz",
+                         quiet="logical"
+                         )
+                      )
+#------------------------------------------------------------------------------------------------------------------------
+setGeneric("dataSummary",     signature="obj", function(obj) standardGeneric("dataSummary"))
+setGeneric("setRegion",       signature="obj", function(obj, targetString) standardGeneric("setRegion"))
+setGeneric("getRegion",       signature="obj", function(obj) standardGeneric("getRegion"))
+setGeneric("loadFootprints",  signature="obj", function(obj) standardGeneric("loadFootprints"))
+
+setGeneric("loadSNPs",        signature="obj", function(obj) standardGeneric("loadSNPs"))
+setGeneric("findSNPsInFootprints", signature="obj", function(obj, tbl.snps, tbl.fp) standardGeneric("findSNPsInFootprints"))
+setGeneric("displayMotif",    signature="obj", function(obj, motifName) standardGeneric("displayMotif"))
+#------------------------------------------------------------------------------------------------------------------------
+trenaViz.PORT.RANGE <- 8000:8020
+# database.host <- "bddsrds.globusgenomics.org"
+database.host <- "whovian"
+#------------------------------------------------------------------------------------------------------------------------
+trenaGWAS01 = function(gwasLocusNumber, targetGene, targetGene.tss, quiet=TRUE)
+{
+   trena <- Trena("hg38", quiet)
+
+   tv <- trenaViz(trenaViz.PORT.RANGE, quiet=quiet)
+   setGenome(tv, "hg38")
+
+   obj <- .trenaGWAS01(gwasLocusNumber=gwasLocusNumber,
+                       targetGene=targetGene, targetGene.tss=targetGene.tss,
+                       trena=trena, trenaViz=tv,
+                       quiet=quiet)
+
+} # trenaGWAS01
+#------------------------------------------------------------------------------------------------------------------------
+setMethod("dataSummary", "trenaGWAS01",
+
+     function(obj){
+        s <- "---- basic data used in this instance of trenaGWAS01";
+        s <- paste(s, "", sep="\n")
+        s <- paste(s, "tbl.loci: ", sep="\n")
+        s <- paste(s, "tbl.credSnp: ", sep="\n")
+        s <- paste(s, "tbl.wg.trena: ", sep="\n")
+        s <- paste(s, "tbl.geneModel.mtx.cer.TSNARE1.250kb: ", sep="\n")
+        cat(s)
+        cat("\n")
+       })
+
+#------------------------------------------------------------------------------------------------------------------------
+setMethod("setRegion", "trenaGWAS01",
+
+     function(obj, targetString){
+        showGenomicRegion(obj@trenaViz, targetString)
+        })
+
+#------------------------------------------------------------------------------------------------------------------------
+setMethod("getRegion", "trenaGWAS01",
+
+     function(obj){
+        parseChromLocString(getGenomicRegion(obj@trenaViz))
+        })
+
+#------------------------------------------------------------------------------------------------------------------------
+# query trenaViz (in the browser) for the currently displayed genomic region
+# then use the trena convenience method "getRegulatoryChromosomalRegions" to get and
+# display brain DHS footprints reported in that region
+#
+# targetGene and targetGene.tss are used so that the resulting footprints can be
+# annotated with respect to the (presumed) gene of interest
+#
+setMethod("loadFootprints", "trenaGWAS01",
+
+    function(obj){
+       dbNames <- c("brain_hint_16", "brain_hint_20", "brain_wellington_20", "brain_wellington_16")
+       brain.hint16.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[1])
+       brain.hint20.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[2])
+       brain.wellington16.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[3])
+       brain.wellington20.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[4])
+       sources <- list(brain.hint16.db.uri, brain.hint20.db.uri,
+                       brain.wellington16.db.uri,brain.wellington20.db.uri)
+
+       names(sources) <- dbNames
+       current.region <- parseChromLocString(getGenomicRegion(obj@trenaViz))
+       printf("span: %d", 1 + current.region$end - current.region$start)
+       tbls <- getRegulatoryChromosomalRegions(obj@trena,
+                                               current.region$chrom, current.region$start, current.region$end,
+                                               sources, obj@targetGene, obj@targetGene.tss)
+       names(tbls) <- dbNames
+       track.count <- length(tbls)
+
+       #colors <- rainbow_hcl(track.count)
+       #colors <- terrain_hcl(track.count)
+       colors <- heat_hcl(track.count)
+       #colors <- sequential_hcl(track.count)
+
+       next.color <- 1
+
+       for(i in seq_len(length(tbls))){
+          track.name <- names(sources)[i]
+          if(track.name %in% getTrackNames(obj@trenaViz))
+             removeTracksByName(obj@trenaViz, track.name)
+          columns.of.interest <- c("chrom", "motifStart", "motifEnd", "motifName")
+
+          tbl <- tbls[[i]]
+          tbls[[i]]$fpSource <- dbNames[i]   # in preparation for combining them
+
+          stopifnot(all(columns.of.interest %in% colnames(tbl)))
+          addBedTrackFromDataFrame(obj@trenaViz, track.name, tbl[, columns.of.interest], color=colors[next.color]);
+          next.color <- next.color + 1
+          } # for i
+
+       tbl.single <- do.call(rbind, tbls)
+       rownames(tbl.single) <- NULL
+       tbl.single
+       }) # addFootprintsInCurrentRegion
+
+#----------------------------------------------------------------------------------------------------
+# both the index snp, and its accompanying imputed ("credible") snps
+setMethod("loadSNPs", "trenaGWAS01",
+
+     function(obj){
+
+        load(system.file(package="trenaGWAS01", "extdata", "tbl.loci.RData"))
+        load(system.file(package="trenaGWAS01", "extdata", "tbl.credSnp.RData"))
+        load(system.file(package="trenaGWAS01", "extdata", "tbl.imputedSnps.chr8.RData"))
+
+           # get the index snps, and all the credible snps, in the current locus
+           # note that this may be different from the currently display region in trenaViz
+        locus.chrom <- tbl.loci$chrom[obj@gwasLocusNumber]
+        locus.start <- tbl.loci$start38[obj@gwasLocusNumber]
+        locus.end   <- tbl.loci$end38[obj@gwasLocusNumber]
+        locusLoc <- sprintf("%s:%d-%d", locus.chrom, locus.start, locus.end)
+
+        tbl.lociSnps <- subset(tbl.credSnp, chrom==locus.chrom & pos38 >= locus.start & pos38 <= locus.end)
+
+        with(unique(tbl.lociSnps[, c("id", "chrom", "pos38")]),
+             addBedTrackFromDataFrame(obj@trenaViz, "indexSNP",
+                                      data.frame(chrom=chrom, start=pos38, end=pos38, stringsAsFactors=FALSE), color="red"))
+
+             # now get their hg38 coordinates.  for now, ignore all imputed snps without rsid
+        imputed.snps <- grep("^rs", tbl.lociSnps$credibleSNP, value=TRUE)
+           # SNPlocs is way too slow.  so for this case study, chr8 snps of interest have been extracted
+           # from that bioc data packages, saved in extdata, loaded above
+           # printf("retrieveing snp locations from Bioconductor package 'SNPlocs.Hsapiens.dbSNP150.GRch38'")
+           # printf("takes a few minutes on the first call, and ~30 seconds on subsequent calls...")
+           # tbl.imputed.snps <- as.data.frame(snpsById(SNPlocs.Hsapiens.dbSNP150.GRCh38, imputed.snps))
+
+        tbl.imputed.snps <- subset(tbl.imputedSnps.chr8, RefSNP_id %in% tbl.credSnp$credibleSNP)
+              # transform tbl.imputed.snps a bit for rendering in trenaViz: rearrange & rename columns
+        tbl.imputedSnp.bed <- tbl.imputed.snps[, c("seqnames", "pos", "pos", "RefSNP_id")]
+        colnames(tbl.imputedSnp.bed) <- c("chrom", "start", "end", "rsid")
+        tbl.imputedSnp.bed$chrom <- as.character(tbl.imputedSnp.bed$chrom)
+        tbl.imputedSnp.bed$chrom <- sprintf("chr%s", tbl.imputedSnp.bed$chrom)
+        current.region <- parseChromLocString(getGenomicRegion(obj@trenaViz))
+        tbl.imputedSnp.bed <- subset(tbl.imputedSnp.bed, start >= current.region$start &
+                                                         end   <= current.region$end &
+                                                         chrom == current.region$chrom)
+        printf("found %d imputed snps in region", nrow(tbl.imputedSnp.bed))
+        addBedTrackFromDataFrame(obj@trenaViz, "imputed", tbl.imputedSnp.bed, color="purple")
+        return(tbl.imputedSnp.bed)
+        }) # loadSNPs
+
+#----------------------------------------------------------------------------------------------------
+setMethod("findSNPsInFootprints", "trenaGWAS01",
+
+     function(obj, tbl.snps, tbl.fp){
+        tbl.fp.simple <- unique(tbl.fp[, 1:4])
+        colnames(tbl.fp.simple) <- c("chrom", "start", "end", "motifName")
+        gr.fp <- GRanges(tbl.fp.simple)
+        gr.snp <- GRanges(tbl.snps)
+        tbl.ov <- as.data.frame(findOverlaps(gr.snp, gr.fp))
+        if(nrow(tbl.ov) == 0){
+           printf("no snps in footprints")
+           return(data.frame())
+           }
+        colnames(tbl.ov) <- c("snp", "fp")
+        tbl.snpFp <- tbl.snps[tbl.ov$snp,]
+        rownames(tbl.snpFp) <- NULL
+        addBedTrackFromDataFrame(obj@trenaViz, "imputedInFP", tbl.snpFp, color="magenta")
+        tbl.snpFp
+        })
+
+#----------------------------------------------------------------------------------------------------
+setMethod("displayMotif", "trenaGWAS01",
+
+     function(obj, motifName){
+        browser()
+        xyz <- 99
+        })
+
+#----------------------------------------------------------------------------------------------------
+# collapse all footprints from multiple sources into one non-redundant, non-overlapping track
+# this may be a useful visualization, as well as the basis for an exhaustive search for binding
+# motifs, using trena's MotifMatcher class
+#
+# the "expandFootprintsBy" parameter will lead to the unification of more individual footprints
+# this will sometimes be useful:  calling footprints is an inexact art; allowing for extra
+# width may help in generating hypotheses - implicating snps - which would otherwise be missed.
+createCollapsedFootprintTrack <- function(expandFootprintsBy=10)
+{
+   dbNames <- c("brain_hint_16", "brain_hint_20", "brain_wellington_20", "brain_wellington_16")
+   brain.hint16.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[1])
+   brain.hint20.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[2])
+   brain.wellington16.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[3])
+   brain.wellington20.db.uri <- sprintf("postgres://%s/%s", database.host, dbNames[4])
+
+   sources <- list(brain.hint16.db.uri, brain.hint20.db.uri, brain.wellington16.db.uri, brain.wellington20.db.uri)
+   names(sources) <- dbNames
+
+   current.region <- parseChromLocString(getGenomicRegion(tv))
+
+   printf("span: %d", 1 + current.region$end - current.region$start)
+   tbls <- getRegulatoryChromosomalRegions(obj@trena,
+                                           current.region$chrom, current.region$start, current.region$end,
+                                           sources, targetGene, targetGene.tss)
+     # make a GRangesList out of the 4 GRanges, converting each data.frame to a GRange object
+   grl <- GRangesList(lapply(tbls, GRanges))
+   gr.all <- Reduce(c, grl)
+   strand(gr.all) <- "*"  # ignore strand differences
+   start(gr.all) <- start(gr.all) - expandFootprintsBy
+   end(gr.all) <- end(gr.all) + expandFootprintsBy
+
+   tbl.fpCollapsed <- as.data.frame(reduce(gr.all))  # metadata discarded
+   trackName <- "fpCollapsed"
+   if(trackName %in% getTrackNames(tv))
+      removeTracksByName(tv, trackName)
+p
+   addBedTrackFromDataFrame(tv, trackName, tbl.fpCollapsed, color="green")
+
+     # find the imputed snp, collapsed fp overlaps
+   #colnames(tmp.bed)[1] <- "seqnames"
+   #tmp.bed$seqnames <- sprintf("chr%s", tmp.bed$seqnames)
+
+   #tmp.bed$seqnames <- "chr8"
+   #tbl.overlaps <- as.data.frame(findOverlaps(GRanges(tmp.bed), GRanges(tbl.fpCollapsed)))
+   #colnames(tbl.overlaps) <- c("snp", "fp")
+   #trackName <- "snpInFp"
+   #if(trackName %in% getTrackNames(tv))
+   #   removeTracksByName(tv, trackName)
+
+   #tbl.snpsInFP <<- tmp.bed[tbl.overlaps$snp,]
+   #addBedTrackFromDataFrame(tv, trackName, tmp.bed[tbl.overlaps$snp,], color="magenta")
+
+   invisible(tbl.fpCollapsed)
+
+} # createCollapsedFootprintTrack
+#----------------------------------------------------------------------------------------------------
+
---
+++ inst/unitTests/test_trenaGWAS01.R
@@ -0,0 +1,87 @@
+library(RUnit)
+library(trenaGWAS01)
+#------------------------------------------------------------------------------------------------------------------------
+printf <- function(...) print(noquote(sprintf(...)))
+#------------------------------------------------------------------------------------------------------------------------
+if(!exists("tgwas"))
+   tgwas <- trenaGWAS01(gwasLocusNumber=5, targetGene="TSNARE1", targetGene.tss=142354831, quiet=TRUE)
+
+#------------------------------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+  test_constructor();
+  test_dataSummary();
+  test_setGetRegion()
+  test_loadFootprintsAndSnps_thenIntersection()
+
+} # runTests
+#------------------------------------------------------------------------------------------------------------------------
+test_constructor <- function()
+{
+   printf("--- test_constructor")
+   checkTrue("trenaGWAS01" %in% is(tgwas))
+
+} # test_constructor
+#------------------------------------------------------------------------------------------------------------------------
+test_dataSummary <- function()
+{
+   printf("--- test_dataSummary")
+   dataSummary(tgwas)
+
+} # test_dataSummary
+#------------------------------------------------------------------------------------------------------------------------
+test_setGetRegion <- function()
+{
+   printf("--- test_dataSummary")
+   setRegion(tgwas, "chr1")
+   Sys.sleep(1)
+   region.at.start <- getRegion(tgwas)
+   checkEquals(region.at.start, list(chrom="chr1", start=1, end=248956422))
+
+   setRegion(tgwas, "TSNARE1")
+   Sys.sleep(1)
+   region.tsnare1 <- getRegion(tgwas)
+   checkEquals(region.tsnare1, list(chrom="chr8", start=142211080, end=142404249))
+
+} # test_setGetRegion
+#------------------------------------------------------------------------------------------------------------------------
+test_loadFootprintsAndSnps_thenIntersection <- function()
+{
+   printf("--- test_loadFootprintsAndSnps_thenIntersection")
+
+   setRegion(tgwas, "chr8:142,230,914-142,234,913") # rs49776977 is at 142,232,793
+
+   system.time(
+      tbl.fp <- loadFootprints(tgwas)
+         )   # about 32 seconds locally to whovian, 192 to bddsrds
+
+   checkEquals(ncol(tbl.fp), 10)
+   checkTrue(nrow(tbl.fp) > 50)
+   expected.colnames <-  c("chrom",  "motifStart",  "motifEnd",  "motifName", "strand",  "score",  "length",
+                           "distance.from.tss","id", "fpSource")
+   checkTrue(all(expected.colnames %in% colnames(tbl.fp)))
+
+   system.time(
+      tbl.snps <- loadSNPs(tgwas)
+      )   # 85 seconds with SNPlocs package, 0.3 seconds with extdata/tbl.imputedSnps.chr8.RData
+
+   tbl.snpsInFp <- findSNPsInFootprints(tgwas, tbl.snps, tbl.fp)
+
+
+
+} # test_loadFootprintsAndSnps_thenIntersection
+#------------------------------------------------------------------------------------------------------------------------
+test_displayMotif <- function()
+{
+   printf("--- test_motif")
+   setRegion(tgwas, "chr8:142,230,914-142,234,913") # rs49776977 is at 142,232,793
+
+
+} # test_loadSNPs
+#------------------------------------------------------------------------------------------------------------------------
+# chr8 142232793      * |   rs4976977  W (A -> T)
+demo_NFIX_snp <- function()
+{
+
+} # demo_NFIX_snp
+#------------------------------------------------------------------------------------------------------------------------
